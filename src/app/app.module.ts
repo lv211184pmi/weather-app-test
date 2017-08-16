6fe4b344805dc08f39a631d583e695b613421a73
Mykhailo Panas <lv211184pmi@gmail.com>
2017-08-16 22:09:18 +0300

    creating project with shared constructions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { WeatherItemComponent } from './weather-item/weather-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherItemComponent
   ],
   imports: [
     BrowserModule,
